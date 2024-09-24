eeb53e810a571c3caee8b9f48464e766a197b2b6
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-09-24 19:28:44 +0530

    Update SpecificMeeting.js

--- models/SpecificMeeting.js
+++ models/SpecificMeeting.js
@@ -61,10 +61,8 @@ const SpecificMeetingSchema = new mongoose.Schema({
     validate: {
       validator: validateImageUrl,
       message: props => `${props.value} is not a valid image URL!`
-    },
-    // Remove required: true to make it optional
+    }
   },
-
   geotagging: { 
     type: String, 
     required: true 
@@ -80,16 +78,13 @@ const SpecificMeetingSchema = new mongoose.Schema({
 
 // Virtual field to calculate the total attendance
 SpecificMeetingSchema.virtual('attendance').get(function() {
-  // attendance is calculated as the number of attendeesName entries
   return this.attendeesName.length;
 });
 
 // Virtual field to calculate attendanceHours based on the formula: attendance x 10 / 60
 SpecificMeetingSchema.virtual('attendanceHours').get(function() {
   const attendance = this.attendeesName.length;
-  // Assuming 10 hours per attendee, divided by 60 to convert to hours
   return (attendance * 10) / 60;
 });
 
-// Export the schema
 module.exports = mongoose.model('SpecificMeeting', SpecificMeetingSchema);
