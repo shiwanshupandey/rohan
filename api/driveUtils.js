dee55dd1347064ff53acca4640e892ee349a7dc5
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-10-04 18:36:48 +0530

    Create driveUtils.js

---
+++ api/driveUtils.js
@@ -0,0 +1,49 @@
+const { google } = require('googleapis');
+const { Readable } = require('stream');
+const { private_key, client_email } = require('./credentials.json');
+
+const oauth2Client = new google.auth.JWT(
+  client_email,
+  null,
+  private_key,
+  ['https://www.googleapis.com/auth/drive']
+);
+
+const drive = google.drive({ version: 'v3', auth: oauth2Client });
+
+// Helper function to convert buffer to stream
+const bufferToStream = (buffer) => {
+  const stream = new Readable();
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+};
+
+// Function to upload file to Google Drive
+const uploadToDrive = async (fileBuffer, fileName, mimeType, folderId) => {
+  const fileMetadata = {
+    name: fileName,
+    parents: [folderId],
+  };
+
+  const media = {
+    mimeType,
+    body: bufferToStream(fileBuffer),
+  };
+
+  const driveResponse = await drive.files.create({
+    resource: fileMetadata,
+    media: media,
+    fields: 'id',
+  });
+
+  // Make file publicly accessible
+  await drive.permissions.create({
+    fileId: driveResponse.data.id,
+    requestBody: { role: 'reader', type: 'anyone' },
+  });
+
+  return `https://drive.google.com/uc?id=${driveResponse.data.id}&export=download`;
+};
+
+module.exports = { uploadToDrive };
