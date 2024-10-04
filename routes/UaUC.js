5aad4b4f2a49b3d31ef54e43891148bc8ec5c92e
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-10-04 18:54:18 +0530

    Update UaUC.js

--- routes/UaUC.js
+++ routes/UaUC.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Observation = require('../models/UaUC');
 const router = express.Router();
 const { uploadToDrive } = require('../api/driveUtils');
+const multer = require('multer');
 
 
 const upload = multer({ storage: multer.memoryStorage() });
